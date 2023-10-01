9e7d112d0f88e17e1d6eaf8d737210cf95cfa2fd
w0122n <146492171+w0122n@users.noreply.github.com>
2023-10-01 15:21:52 -0600

    Add files via upload

---
+++ Unidad 2/Prom_6.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+
+namespace P_6 
+{
+    internal class Grados
+    {
+        static void Main(string[] args)
+        {
+            int C, F;
+            Console.WriteLine("Introduce el valor de los grados centigrados");
+            C = Convert.ToInt32(Console.ReadLine());
+            F = (C * 9/5) + 32;
+            Console.WriteLine("Los grados fahrenheit son :" + F);
+
+        }
+    }
+}
---
+++ Unidad 2/Prom_9.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P_9 // Note: actual namespace depends on the project name.
+{
+    internal class Calculadora
+    {
+        static void Main(string[] args)
+        {
+            int a, b, s, r, m, d;
+            Console.WriteLine("Introduce primer valor");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce segundo valor");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            s = a + b;
+            Console.WriteLine("La suma es igual a :" + s);
+            r = a - b;
+            Console.WriteLine("La resta es igual a :" + r);
+            m = a * b;
+            Console.WriteLine("La multiplicación es igual a :" + m);
+            d = a / b;
+            Console.WriteLine("La división es igual a :" + d);
+        }
+    }
+}
