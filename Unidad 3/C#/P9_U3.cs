82652c49668f64e60f32f6837790732a4ba11aa5
w0122n <L23181358@slp.tecnm.mx>
2023-10-19 01:13:03 -0600

    nuevos

---
+++ Unidad 3/C#/P9_U3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P9_U3 // Note: actual namespace depends on the project name.
+{
+    internal class Estacionamiento
+    {
+        static void Main(string[] args)
+        {
+            int he, hs, ht;
+            double costo = 0;
+
+            Console.WriteLine("Utiliza el formato de 24 horas.");
+            Console.WriteLine("¿Cual fue la hora de entrada? ");
+            he = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("¿Cual fue la hora de salida? ");
+            hs = Convert.ToInt32(Console.ReadLine());
+
+            ht = hs - he;
+
+            if (ht <= 2)
+            {
+                costo = ht * 5;
+            }
+
+            else if (ht <= 5)
+            {
+                costo = ht * 4;
+            }
+
+
+        }
+    }
+}
