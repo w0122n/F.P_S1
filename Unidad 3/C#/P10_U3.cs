b0ac5c37e4ebd840a8c51909708459c6772ad084
w0122n <L23181358@slp.tecnm.mx>
2023-10-20 02:52:05 -0600

    6-10

---
+++ Unidad 3/C#/P10_U3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hrtrab, hrext;
+            double pago, sueldosem = 0;
+            Console.WriteLine("¿Cuantas horas trabajaste?");
+            hrtrab = Convert.ToInt32(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("¿Cuanto es el pago por hora?");
+            pago = Convert.ToInt32(Convert.ToInt32(Console.ReadLine()));
+
+            if (hrtrab <= 40 )
+            {
+                sueldosem = hrtrab * pago;
+                Console.WriteLine("Recibiras un total de $" + sueldosem);
+            }
+            else
+            {
+                hrext = hrtrab - 40;
+                sueldosem = 40 * pago + hrext * (pago * 2);
+                Console.WriteLine("Por trabajar " + hrext + " horas extra, usted recibe un total de $" + sueldosem);
+
+
+            }
+        }
+    }
+}
