38208ce45e98c8dc197aca11f12b1d2558bf4a26
w0122n <146492171+w0122n@users.noreply.github.com>
2023-11-06 17:22:21 -0600

    c

---
+++ Unidad 3/C#/P22_U3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P22_U3 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, num, s = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese un valor: (Ingrese 0 para terminar)");
+                num = Convert.ToInt32(Console.ReadLine());
+                s = s + num;
+                Console.WriteLine(s);
+
+            } while (num != 0);
+        }
+    }
+}
---
+++ Unidad 3/C#/P23_U3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P23_U3 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n <= 10);
+        }
+    }
+}
