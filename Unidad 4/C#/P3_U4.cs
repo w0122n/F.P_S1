36dc3a7091dbbd62f03e44755c68f38a72fcea24
w0122n <146492171+w0122n@users.noreply.github.com>
2023-11-09 17:52:28 -0600

    c

---
+++ Unidad 4/C#/P3_U4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Temperatura
+    {
+        static void Main(string[] args)
+        {
+
+            double f = 0, k = 0;
+            int temp;
+            Console.WriteLine("Ingresa las temperaturas procesar");
+            temp = Convert.ToInt32(Console.ReadLine());
+
+            int[] cent = new int[temp];
+
+            for (int c = 0; c < cent.Length; c++)
+            {
+                Console.WriteLine("Escribe la temperatura en indice " + c + ": ");
+                cent[c] = Convert.ToInt32(Console.ReadLine());
+
+                f = (cent[c] * 9 / 5) + 32;
+
+                Console.WriteLine("Los grados Farenheit son " + f);
+
+                k = cent[c] + 273.15;
+
+                Console.WriteLine("Los grados Kelvin son " + k);
+
+            }
+        }
+    }
+}
