4ebcc81451a22545de7652bdb64bf4479f1a68ea
w0122n <146492171+w0122n@users.noreply.github.com>
2023-11-08 21:36:13 -0600

    Add files via upload

---
+++ Unidad 4/C#/P2_U4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Promedio
+    {
+        static void Main(string[] args)
+        {
+
+            /* int[] calif = new int[10] { 90, 83, 75, 62, 91, 80, 66, 90, 79, 87 };
+
+            double sum = 0, prom = 0;
+
+            for (int c = 0; c < 10; c++)
+            {
+                sum = sum + calif[c];
+                prom = sum / 10;
+
+            }
+            Console.WriteLine("Tu promedio es " + prom); */
+
+
+            int sum = 0, prom = 0, tam;
+            Console.WriteLine("Ingresa las calificaciones a procesar");
+            tam = Convert.ToInt32(Console.ReadLine());
+
+            int[] calf = new int[tam];
+
+            for (int c = 0; c < calf.Length; c++)
+            {
+                Console.WriteLine("Escribe calificación en indice " + c + ": ");
+                calf[c] = Convert.ToInt32(Console.ReadLine());
+
+            }
+        }
+    }
+}
