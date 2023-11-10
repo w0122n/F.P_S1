85bc38bcc2f69b02ee4ce16892a754c9301b1979
w0122n <146492171+w0122n@users.noreply.github.com>
2023-11-09 18:09:17 -0600

    c

---
+++ Unidad 4/C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Negativo
+    {
+        static void Main(string[] args)
+        {
+
+            int n=0, total=0, num;
+            Console.WriteLine("Ingresa los números a procesar");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            int[] neg = new int[num];
+
+            for (int ne = 0; ne < neg.Length; ne++)
+            {
+                Console.WriteLine("Ingrese número negativo");
+                neg[ne] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Hayn un total de ");
+        }
+    }
+}
