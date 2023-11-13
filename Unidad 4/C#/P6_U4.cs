c5c35506f7eaaa6035c10bfdc65f42aa9859f63f
w0122n <146492171+w0122n@users.noreply.github.com>
2023-11-12 18:32:18 -0600

    C

---
+++ Unidad 4/C#/P6_U4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n;
+
+            Console.WriteLine("Ingrese los números a procesar");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] num = new int[n];
+
+            for (int i=0; i<num.Length; i++) 
+            {
+                Console.WriteLine("Escribe un número en indice " + n + ": "); ;
+                num[n] = Convert.ToInt32(Console.ReadLine());
+            } 
+            Console.WriteLine("El orden de los números ");
+        }
+    }
+}
