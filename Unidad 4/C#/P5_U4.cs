8bdfc89a8da195af3743d18c9544492fc95228f9
w0122n <146492171+w0122n@users.noreply.github.com>
2023-11-13 18:07:37 -0600

    C

--- Unidad 4/C#/P5_U4.cs
+++ Unidad 4/C#/P5_U4.cs
@@ -11,15 +11,22 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("Ingrese las frutas a procesar: ");
             f = Convert.ToInt32(Console.ReadLine());
 
-            int[] frutas = new int[f];
+            string[] frutas = new string[f];
 
             for (int i = 0; i < frutas.Length; i++)
             {
-                Console.WriteLine("Escribe una fruta en indice " + i + ": ");
-                frutas[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escribe una fruta en indice " + (i+1) + ": ");
+                frutas[i] = Console.ReadLine();
+                
             }
-
-
+            foreach (string F in frutas)
+            {
+                Console.WriteLine(F);
+            }
+            for (int j = f-1; j >=0; j--)
+            {
+                Console.WriteLine(frutas[j]);
+            }
         }
     }
 }
