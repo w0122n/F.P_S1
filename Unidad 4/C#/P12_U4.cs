5491f1dd9218eb09d23fcd93013769b3224066b2
w0122n <146492171+w0122n@users.noreply.github.com>
2023-11-14 18:01:59 -0600

    new

---
+++ Unidad 4/C#/P12_U4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = { { 21, 5, 21, 5 }, { 25, 3, 18, 8 }, { 22, 1, 6, 17 } };
+
+            int fila = num.GetLength(0);
+            int col = num.GetLength(1);
+
+            for (int i = 0; i < fila; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(num[i, j] + " ");
+                }
+                Console.WriteLine();    
+            }
+            
+            /* int[,] num = { { 21, 5, 21 }, { 25, 3, 18 }, { 22, 1, 6 } };
+
+            for (int i = 0; i <=2; i++)
+            {
+                for (int c = 0; c <=2; c++)
+                {
+                    Console.Write(num[i,c] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write(num[0, 0]);
+            Console.Write(num[0, 1]);
+            Console.Write(num[0, 2]);
+            Console.WriteLine();
+            Console.Write(num[1, 0]);
+            Console.Write(num[1, 1]);
+            Console.Write(num[1, 2]);
+            Console.WriteLine();
+            Console.Write(num[2, 0]);
+            Console.Write(num[2, 1]);
+            Console.Write(num[2, 2]);*/
+        }
+    }
+}
